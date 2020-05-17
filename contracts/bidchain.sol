80585c64c26a0b739b3e0cb8f2693093cf7bf77d
KevinMadoka <k478610003910@gmail.com>
2020-05-17 14:02:29 +0800

    Adding pay & checking mechanism

--- contracts/bidchain.sol
+++ contracts/bidchain.sol
@@ -1,62 +1,129 @@
-//pragma solidity >=0.5.0 <0.6.0;       // Not works
-pragma experimental ABIEncoderV2;       // It seems that using user-defined struct objects must use this compiler version
+//pragma solidity >=0.5.0 <0.6.0;   // Not works
+pragma experimental ABIEncoderV2;   // It seems that using user-defined struct objects must use this compiler version
 
 //import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Bidchain {
 
-    event NewAuction(uint32 auctionId, uint32 endTime, uint32 beginPrice, address seller, string[] itemInfo);
-    event Bidding(uint32 auctionId, uint32 endTime, uint32 newPrice, address bidder);
+    event NewAuction(uint32 auctionId, uint32 endTime, uint256 beginPrice, address seller, string[] itemInfo);
+    event Bidding(uint32 auctionId, uint32 endTime, uint256 newPrice, address bidder);
+    event Released(uint32 auctionId);
+    event Locked(uint32 auctionId);
+    event Waited(uint32 auctionId);
+    event Closed(uint32 auctionId);
+
 
     struct ItemInfo {
         string name;                // Item's name
         string descript;            // Item description
         string imageURL;            // Maybe imgur URL
     }
+
+/*
+ * State object is Created if created but no one bidding
+ * State object is Active if someone bidding and the time is not ended
+ * State object is Locked after time ended and buyer not confirmed
+ * State object is Inactive if buyer confirmed or time ended and no one bidding
+ */
+    enum State { Created, Active, Waited, Locked, Released, Closed }
+
     struct Auction {
-        uint32      endTime;        // Auction will close after endTime
-        uint32      price;          // Current offered max price
-        address     seller;         // Seller's account address
-        address     winner;         // After auction closed, the max price's account address
-        ItemInfo    itemInfo;
+        uint32              endTime;    // Auction will close after endTime
+        uint256             price;      // Current offered max price
+        address payable     seller;     // Seller's account address
+        address payable     winner;     // After auction closed, the max price's account address
+        ItemInfo            itemInfo;
+        State               state;
     }
 
     Auction[] public auctions;
-    mapping (address => uint32[]) public seller2Auction;
+    mapping (address => uint32[])   public seller2Auction;
+    mapping (uint32 => address)     public auction2Seller;
 
+    modifier onlySeller(uint32 auctionId) {
+        require(msg.sender == auction2Seller[auctionId], "Only seller can call this!");
+        _;
+    }
+
+    modifier onlyWinner(uint32 auctionId) {
+        require(msg.sender == auctions[auctionId].winner, "Only winner can call this!");
+        _;
+    }
+
+    modifier validAuction(uint32 period, uint32 infoNum) {
+        require(period <= 3 days && infoNum == 3, "Invalid auction creation!");
+        _;
+    }
+    modifier validBidding(uint32 auctionId, uint256 newPrice) {
+        require(auctionId < auctions.length &&
+                (auctions[auctionId].state == State.Created || auctions[auctionId].state == State.Active) &&
+                newPrice > auctions[auctionId].price, "Invalid bidding!");
+        _;
+    }
 
 /*
  * Create the new auction by seller,
  * you can access seller's auction by first query seller2Auction[seller-address],
  * then auctions[seller2Auction[seller-address][specify-seller's-auction-id]]
  */
-    function createAuction(uint32 period, uint32 beginPrice, string[] calldata itemInfo) external returns(bool) {
-        if (itemInfo.length != 3 || period > 3 days)
-            return false;
-        uint32 id = uint32(auctions.push(Auction(uint32(now) + period, beginPrice, msg.sender, address(0), ItemInfo(itemInfo[0], itemInfo[1], itemInfo[2])))) - 1;
+    function createAuction(uint32 period, uint256 beginPrice, string[] calldata itemInfo) external validAuction(period, uint32(itemInfo.length)) {
+        uint32 id = uint32(auctions.push(Auction(uint32(now) + period, beginPrice, msg.sender, address(0), ItemInfo(itemInfo[0], itemInfo[1], itemInfo[2]), State.Created))) - 1;
         seller2Auction[msg.sender].push(id);
+        auction2Seller[id] = msg.sender;
         emit NewAuction(id, uint32(now) + period, beginPrice, msg.sender, itemInfo);
-        return true;
-    }
-    
-    function bidding(uint32 auctionId, uint32 newPrice) external returns(bool) {
-        // Checking auction exists
-        if (auctionId >= auctions.length)
-            return false;
-        // Checking whether auction has closed or newPrice is less than current price
-        // Here we can decide the price's offset
-        if (uint32(now) > auctions[auctionId].endTime || newPrice <= auctions[auctionId].price)
-            return false;
-        auctions[auctionId].winner  = msg.sender;
-        auctions[auctionId].price   = newPrice;
-        emit Bidding(auctionId, auctions[auctionId].endTime, newPrice, msg.sender);
     }
 
+    function bidding(uint32 auctionId) external payable validBidding(auctionId, msg.value) {
+        require(msg.sender.balance >= msg.value, "Insufficient token!");
+        if (auctions[auctionId].state == State.Active)
+            auctions[auctionId].winner.transfer(auctions[auctionId].price);  // Refund to the original bidder
+        auctions[auctionId].winner  = msg.sender;
+        auctions[auctionId].price   = msg.value;
+        auctions[auctionId].state   = State.Active;
+        emit Bidding(auctionId, auctions[auctionId].endTime, msg.value, msg.sender);
+    }
+
+
+    // Checking the time up auctions and emit event
+    function checking() external {
+        for (uint32 i = 0; i < auctions.length; i++) {
+            if ((auctions[i].state == State.Created || auctions[i].state == State.Active) && auctions[i].endTime > uint32(now)) {
+                if (auctions[i].state == State.Created) {
+                    auctions[i].state = State.Closed;
+                    emit Closed(i);
+                }
+                else {
+                    auctions[i].state = State.Waited;
+                    emit Waited(i);
+                }
+            }
+        }
+    }
+
+    function locking(uint32 auctionId) external payable onlySeller(auctionId) {
+        require(auctions[auctionId].state == State.Waited, "Invalid state!");
+        require(msg.sender.balance >= msg.value && msg.value == auctions[auctionId].price, "Insufficient token!");
+        auctions[auctionId].state = State.Locked;
+        emit Locked(auctionId);
+    }
+
+    function confirm(uint32 auctionId) external onlyWinner(auctionId) {
+        require(auctions[auctionId].state == State.Locked, "Invalid state!");
+        auctions[auctionId].state = State.Released;
+        emit Released(auctionId);
+    }
+
+    function release(uint32 auctionId) external onlySeller(auctionId) {
+        require(auctions[auctionId].state == State.Released, "Invalid state!");
+        auctions[auctionId].seller.transfer(2*auctions[auctionId].price);
+        auctions[auctionId].state = State.Closed;
+        emit Closed(auctionId);
+    }
     // Getter and Setter
     function getAuctions() external view returns (Auction[] memory) {
         return auctions;
     }
-    function getPrice(uint32 auctionId) external view returns(uint32) {
+    function getPrice(uint32 auctionId) external view returns(uint256) {
         require(auctionId < auctions.length);
         return auctions[auctionId].price;
     }
@@ -90,6 +157,7 @@ contract Bidchain {
         require(newItemInfo.length == 3);
         auctions[auctionId].itemInfo = ItemInfo(newItemInfo[0], newItemInfo[1], newItemInfo[2]);
     }
+
 
 }
 
